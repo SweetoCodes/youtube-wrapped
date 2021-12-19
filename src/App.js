1561e571a271a97c88858d5b0501726a3e54294c
SweetoCodes <charliesweeting18@gmail.com>
2021-12-19 13:35:42 +0000

    init react chrome extension

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/*global chrome*/
+import "./App.css";
+
+function App() {
+  return (
+    <div className=" h-[600px] w-[800px]">
+      <p className="text-3xl font-bold">Init</p>
+    </div>
+  );
+}
+
+export default App;
