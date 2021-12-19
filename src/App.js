4f4bc854dc728a6b5d37887e509180bca7a7ba37
SweetoCodes <charliesweeting18@gmail.com>
2021-12-19 13:38:13 +0000

    added chrome history api call logic and updated readme

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ function App() {
   return (
     <div className=" h-[600px] w-[800px]">
       <p className="text-3xl font-bold">Init</p>
+      <button
+        onClick={() => {
+           chrome.history.search({
+                  text: '',
+                  maxResults: 1000000, //defaults to 100, set arbitrarily high to get all results
+                  startTime:1 // defaults to one day, this gets the full 3 onth history
+              }, function(results) {
+                  console.log(results);
+              })
+        }}
+      >Click for logging</button>
     </div>
   );
 }
